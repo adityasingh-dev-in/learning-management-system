ff61cbda91f55cac23662c09bb86d25e72f18160
adityasingh-dev-in <adityasingh.dev.in@gmail.com>
2026-03-17 19:17:16 +0530

    initial setup

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,12 +1,34 @@
 import express,{Application, urlencoded} from 'express'
 import cookieParser from 'cookie-parser';
+import cors from 'cors'
+import { Response, Request } from 'express';
 
 const app: Application = express();
+
+const allowedOrigins = [
+  'http://localhost:5173',      // Local React/Vite dev server 
+];
+const corsOption = {
+    origin: (origin: any,callback: any)=>{
+        if( !origin || allowedOrigins.includes(origin)){
+            callback(null,true);
+        }else{
+            callback(new Error("Not Allowed By cors"));
+        }
+    },
+    Credential: true,
+    methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH'],
+    allowedHeaders: ['Content-Type', 'Authorization'],
+    optionsSuccessStatus: 200
+}
+
+app.use(cors(corsOption));
 
 
 app.use(express.json({ limit:'20kb'}));
 app.use(urlencoded({ extended: true, limit: '20kb'}));
 app.use(express.static('public'));
 app.use(cookieParser())
+
 
 export default app;
--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,11 +1,8 @@
 import express, { Application, Request, Response } from 'express';
 import app from './app'
 
-app.get('/', (req: Request, res: Response) => {
-  res.send('Hello World with TypeScript and Express!');
-});
-
 const PORT = process.env.PORT || 5000;
+
 app.listen(PORT, () => {
   console.log(`Server is running on http://localhost:${PORT}`);
 });
